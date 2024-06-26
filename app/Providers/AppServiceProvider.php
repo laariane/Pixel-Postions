69bf4adee1ade2b576e2667b6758032754cea549
laariane <laarianeamine@gmail.com>
2024-06-26 02:08:10 +0200

    adding components and layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,13 +14,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
-    }
-
+}
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading();
+
     }
 }
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,21 @@
+
+<div class="p-5 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="mb-2 self-start text-sm">
+        laracasts
+    </div>
+
+    <div class="space-y-3 py-8 font-bold">
+        <h3>Laravel Developer</h3>
+        <p>Full time -  from $60,000</p>
+    </div>
+    <div class="flex justify-between text-xs items-center mt-auto ">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <div>
+            <img class="rounded-xl" src="http://placehold.it/42/42" >
+        </div>
+    </div>
+</div>
